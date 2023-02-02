4338015e36e2c76ca8c83e50ccb4cbf465f99756
prabuhadi <prabuhadiyo@gmail.com>
2023-02-02 20:33:07 +0700

    Create arenaSweep() and collide()

---
+++ script.js
@@ -0,0 +1,34 @@
+"use strict";
+const canvas = document.getElementById("tetris");
+const context = canvas.getContext("2d");
+context.scale(20, 20);
+
+function arenaSweep() {
+  let rowCount = 1;
+  outer: for (let y = arena.length; y > 0; y--) {
+    for (let x = 0; x < arena[y].length; x++) {
+      if (arena[y][x] === 0) {
+        continue outer;
+      }
+    }
+
+    const row = arena.splice(y, 1)[0].fill(0);
+    arena.unshift(row);
+    y++;
+    player.score += rowCount * 10;
+    rowCount *= 2;
+  }
+}
+
+function collide(arena, player) {
+  const m = player.matrix;
+  const o = player.pos;
+  for (let y = 0; y < m.length; y++) {
+    for (let x = 0; x < m[y].length; x++) {
+      if (m[f][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
